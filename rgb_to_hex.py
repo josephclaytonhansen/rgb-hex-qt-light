54a66619ddfcd6da8184729d953bd7f29a93b3d2
Joseph Hansen <45859835+josephclaytonhansen@users.noreply.github.com>
2021-09-21 13:44:56 -0600

    Update rgb_to_hex.py

--- rgb_to_hex.py
+++ rgb_to_hex.py
@@ -3,10 +3,8 @@ Lightweight QT application for converting RGB to HEX and vice versa.
 You can do this more quickly, and with more options, using Google,
 so I wouldn't consider this any kind of production module.
 
-Uses the HEXABIN API- which was the main motivation for this- I needed
-practice with the requests module.
-
-You can find this API at https://hexabin.herokuapp.com/api
+This was using the HEXABIN API, which stopped working on 9/25/21, so
+I've switched over fully to The Color API.
 
 Made by Joseph Hansen for learning purposes,
 do whatever the heck you want with it.
@@ -57,36 +55,20 @@ def rgb_to_hex(r,g=None,b=None):
         r = r[0]
     try:
         rgb = [r,g,b]
-        hex_str = ""
-        hex_arr = []
-        for color in rgb:
-            response = requests.get("https://hexabin.herokuapp.com/api/decimal/"+str(color)+"/convert/hex")
-            h = response.json()["result"]
-            if response.status_code != 200:
-                h = "00"
-            if len(h) == 1:
-                h = "0" + h
-            hex_arr.append(h)
-        hex_str = "".join(hex_arr)
-        return "#" + hex_str
+        p = ",".join([str(r), str(g), str(b)])
+        response = requests.get("https://www.thecolorapi.com/id?", params = {"rgb":p})
+        h = response.json()
+        return h["hex"]["value"]
     except Exception as e:
-        return e
+        print(e)
   
 def hex_to_rgb(h):
     try:
         if h.startswith("#"):
             h = h[1:]
-        h_arr = [h[0:2],h[2:4], h[4:6]]
-        rgb_arr = []
-        for color in h_arr:
-            if color.startswith("0"):
-                color = color[1]
-            response = requests.get("https://hexabin.herokuapp.com/api/hex/"+str(color)+"/convert/decimal")
-            h = response.json()["result"]
-            if response.status_code != 200:
-                h = 0
-            rgb_arr.append(h)
-        return rgb_arr
+        response = requests.get("https://www.thecolorapi.com/id?", params = {"hex":h})
+        h = response.json()
+        return [h["rgb"]["r"], h["rgb"]["g"],h["rgb"]["b"]]
     except Exception as e:
         return e
 
@@ -100,16 +82,15 @@ class main(QMainWindow):
         self.initUI()
     
     def initUI(self):
-        self.setStyleSheet("background:rgba(130,130,130,.5);")
+        self.setStyleSheet("background:rgba(180,180,180,.4);")
         #add color block, RGB fields with button, and HEX field with button
         self.content = QWidget()
-        self.setStyleSheet("background:rgba(130,130,130,.5);")
         self.setCentralWidget(self.content)
         self.content_layout = QGridLayout()
         self.content.setLayout(self.content_layout)
         #I'm not typing that again if I can help it
         c = self.content_layout
-        self.c_block = ColorBlock("#000000", 70, 70, self)
+        self.c_block = ColorBlock("#000000", 80, 80, self)
         c.addWidget(self.c_block,1,1,2,2)
         self.r_entry, self.g_entry, self.b_entry, self.rgb_go = QSpinBox(), QSpinBox(), QSpinBox(), QPushButton("Convert RGB")
         self.r_entry.setRange(0,255)
@@ -120,39 +101,67 @@ class main(QMainWindow):
         c.addWidget(self.b_entry, 1,5,1,1)
         c.addWidget(self.rgb_go, 1,6,1,1)
         self.hex_entry = QLineEdit()
-        c.addWidget(self.hex_entry, 2,3,1,4)
+        c.addWidget(self.hex_entry, 2,3,1,3)
         #update colorblock color with HEX value (or RGB if convert is clicked)
         self.hex_entry.returnPressed.connect(self.colorChangeHex)
-        self.hex_entry.setPlaceholderText("Hex color (press Return to convert)")
+        self.hex_entry.setPlaceholderText("Hex color")
         self.rgb_go.clicked.connect(self.colorChangeRgb)
+        #color name, shades, and scheme
+        self.color_name = QPushButton()
+        self.color_name.clicked.connect(self.closestNamedHex)
+        c.addWidget(self.color_name, 2,6,1,1)
     
     def colorChangeHex(self):
-        self.c_block_pixmap = QPixmap(70,70)
+        self.c_block_pixmap = QPixmap(80,80)
         #update colorblock color with HEX value
-        self.update_value = self.sender().text()
+        self.update_value = self.hex_entry.text()
+        print("update value:"+self.update_value)
         self.rgb = hex_to_rgb(self.update_value.strip())
         if len(self.rgb) == 3:
             self.r_entry.setValue(self.rgb[0])
             self.g_entry.setValue(self.rgb[1])
             self.b_entry.setValue(self.rgb[2]) 
         try:
+            if self.update_value.startswith("#") == False:
+                self.update_value = "#" + self.update_value
             self.c_block_pixmap.fill(QColor(self.update_value))
+            self.updateName()
         except:
             self.c_block_pixmap.fill(QColor("black"))
         self.c_block.setPixmap(self.c_block_pixmap)
+        self.hex = self.update_value
+        self.updateName()
     
     def colorChangeRgb(self):
         #if Convert clicked, do that
         self.rgb = [self.r_entry.value(), self.g_entry.value(), self.b_entry.value()]
         self.hex = rgb_to_hex(self.rgb)
-        self.c_block_pixmap = QPixmap(70,70)
+        self.c_block_pixmap = QPixmap(80,80)
         try:
             self.c_block_pixmap.fill(QColor(self.hex))
             self.hex_entry.setText(self.hex)
-        except:
+            self.updateName()
+        except Exception as e:
+            print(e)
             self.c_block_pixmap.fill(QColor("black"))
         self.c_block.setPixmap(self.c_block_pixmap)
+        self.updateName()
+    
+    def updateName(self):
+        if hasattr(self, "hex"):
+            response = requests.get("https://www.thecolorapi.com/id?", params = {"hex":self.hex}).json()
+            self.relevant_values = {"name":response["name"]["value"],
+                                    "cnh":response["name"]["closest_named_hex"],
+                                    "distance":response["name"]["distance"],
+                                    "contrast":response["contrast"]["value"]}
+            self.color_name.setStyleSheet("background:"+self.hex+";"+"color:"+self.relevant_values["contrast"])
+            self.color_name.setText(self.relevant_values["name"] + " ~"+str(self.relevant_values["distance"]))
+    
+    def closestNamedHex(self):
+        self.hex_entry.setText(self.relevant_values["cnh"])
         
+        self.colorChangeHex()
+    
     def center(self):
         self.setGeometry(
     QStyle.alignedRect(
